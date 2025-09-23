4bb78c2877425da8dafd1ff6bc259a57ebf4a406
Ferspinoza <fer.espinoza.jara@gmail.com>
2025-09-22 23:19:21 -0300

    Create SanityTest.java

---
+++ src/test/java/com/platinum/SanityTest.java
@@ -0,0 +1,7 @@
+package com.platinum;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SanityTest {
+  @Test void passes() { assertTrue(true); }
+}
